5af458aa721d8aba976f56ef894c33a5d620a48f
hristowa <renkagh55@gmail.com>
2021-07-23 19:05:19 +0300

    initial commit

---
+++ Challange 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Challange_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+            //int[] lis;
+            int[] len = new int[array.Length];
+            int[] prev = new int[array.Length];
+            int maxLength = 0;
+            int lastIndex = -1;
+
+            for (int current = 0; current < array.Length; current++)
+            {
+                len[current] = 1;
+                prev[current] = -1;
+                for (int prevIndex = 0; prevIndex < current; prevIndex++)
+                {
+                    if (array[prevIndex] < array[current] && len[prevIndex] >= len[current])
+                    {
+                        len[current] = 1 + len[prevIndex];
+                        prev[current] = prevIndex; //индекс на най-добрия елемент от поредицата
+                    }
+                }
+                //макс брой елементи
+                if (len[current] > maxLength)
+                {
+                    maxLength = len[current];
+                    lastIndex = current;
+                }
+            }
+            int[] list = new int[maxLength];
+            for (int i = 0; i < maxLength; i++)
+            {
+                list[i] = array[lastIndex];
+                lastIndex = prev[lastIndex];
+            }
+            Array.Reverse(list);
+            Console.WriteLine(string.Join(" ", list));
+
+        }
+
+
+    }
+}
---
+++ Staircase Challange/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Staircase_Challange
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int stairs = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Number of ways = {countWays(stairs)}");
+        }
+        /*There's a staircase with N steps, and you can climb 1 or 2 steps at a time.
+        Given N, write a function that returns the number of unique ways you can climb the staircase.*/
+        static int findSteps(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            int result = findSteps(n - 1) + findSteps(n - 2);
+            return result;
+
+        }
+
+        static int countWays(int stairs)
+        {
+            //the value of ways(n) is equal to fibonacci(n+1)
+            return findSteps(stairs + 1);
+        }
+
+        /* What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X?
+         For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time. 
+         Generalize your function to take in X*/  
+    }
+
+}
